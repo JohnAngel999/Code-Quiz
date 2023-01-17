1a9e3a64022f3dc462acce78b0812faae753ac7c
john angel <johnrobertangel04@icloud.com>
2023-01-17 11:46:05 -0800

    finishing js

--- assets/questions.js
+++ assets/questions.js
@@ -23,8 +23,44 @@ incorrectAns.textContent = 'Previous Question: Wrong!'
 
 //create question objects
 var questionList = [
-    { question:
+    { question: 'Commonly used data types Do NOT include:', answerOne: 'Strings',
+    answerTwo: 'Boolean', answerThree: 'Alerts', answerFour: 'Numbers', solution:'question-btn-three'},
 
+    { question: 'The condition in an if / else statement is enclosed with:',
+    answerOne: 'Quotes', answerTwo: 'Curly brackets', answerThree: 'Parenthesis', 
+     answerFour:'Square brackets', solution: 'question-btn-two',
+    },
+    
+    {
+        question: 'Arrays can be used to store:', answerOne:'Numbers and Strings', answerTwo: 'Other Arrays',
+        answerThree:'Booleans', answerFour: 'All of the above', solution:'question-btn-four'
+    },
+
+    {
+        question: 'String values must be enclosed within __ when being assigned to variables.', answerOne: 'Quotes',
+        answerTwo:'Curly brackets', answerThree:'Parenthesis', answerFour:'Square Brackets', solution: 'question-btn-one'
+    },
+
+    {
+        question:'A very useful tool used during development and debugging for printing content to the debugger is: ',
+        answerOne: 'JavaScript', answerTwo:'Terminal/Bash', answerThree:'For Loops', answerFour: 'Console Log', solution:'question-btn-four'
     }
+];
 
-]
+//create question HTML elements
+var questionHead = document.createElement('h1'); //h1 element
+questionHead.className = 'question-head';
+var questionDiv = document.createElement('ol'); //ordered list element
+questionDiv.className = 'question-list'
+var questionBtnOne = document.createElement('button'); //button element
+questionBtnOne.className ='question-btn-one'
+questionBtnOne.id = 'question-btns'
+var questionBtnTwo =document.createElement('button'); //button element
+questionBtnTwo.className ='question-btn-two'
+questionBtnTwo.id = 'question-btns'
+var questionBtnThree = document.createElement('button'); //button element
+questionBtnThree.className = 'question-btn-three'
+questionBtnThree.id = 'question-btns'
+var questionBtnFour =document.createElement('button'); //button element
+questionBtnFour.className = 'question-btn-four'
+questionBtnFour.id ='question-btns'
